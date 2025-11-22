0cd90816355b3b8550b1855b929a5b4a0a386b6e
MradulGandhi <gandhisir80@gmail.com>
2025-11-22 20:16:07 +0530

    Twilio Fixations

--- src/config/twilio.js
+++ src/config/twilio.js
@@ -1,7 +1,6 @@
-const accountSid = process.env.TWILIO_ID ;
-const authToken = process.env.TWILIO_TOKEN ;
+
+const accountSid = process.env.TWILIO_ID;
+const authToken = process.env.TWILIO_TOKEN;
 const client = require('twilio')(accountSid, authToken);
-
-
 
 module.exports = client;
